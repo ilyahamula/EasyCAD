86d4e7c3584159a1dd84a086cf90a6daafecfede
ilyahamula <ilyahamula95@gmail.com>
2018-10-06 19:26:57 +0300

    remove unused member

--- EasyCAD/cAPP.cpp
+++ EasyCAD/cAPP.cpp
@@ -2,21 +2,14 @@
 
 cAPP::cAPP()
 {
-  m_dynArr = new int[45];
 }
 
 cAPP::~cAPP()
 {
-  delete m_dynArr;
 }
 
 cAPP& cAPP::Instance()
 {
   static cAPP app;
   return app;
-}
-
-int* cAPP::GetArr()
-{
-  return m_dynArr;
 }
--- EasyCAD/cAPP.h
+++ EasyCAD/cAPP.h
@@ -12,7 +12,6 @@ class cAPP
 
 public:
   static cAPP& Instance();
-  int* GetArr();
 
 private:
 
